e86a25d5aa766427b7af92cfec394a01d730cb98
Thomas Engler <Thomas Engler@ThomasEngler-PC.localdomain>
2017-12-03 00:08:42 +0100

    Initial Commit.

---
+++ GW_Tracker/GW_Tracker/src/main/java/de/tse/GW_Tracker/App.java
@@ -0,0 +1,13 @@
+package de.tse.GW_Tracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
